570362a2e81393df2764b751de7acca13f30fa73
nctorresl26 <30515755+nctorresl26@users.noreply.github.com>
2019-02-05 22:38:09 -0500

    Producto

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ function toggleMenu() {
         menu.style.display = "none";
     }
 }
+const init = function(){
+	let items = document.querySelectorAll('section');
+	for (let i = 0; i < items.length; i++){
+		items[i].style.background = randomColor({luminosity: 'light'});
+	}
+	cssScrollSnapPolyfill()
+}
+init();
